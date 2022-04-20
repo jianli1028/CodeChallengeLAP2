214538e01e02f6285f41d8db51d8fe6f39c931f1
jianli1028 <96356376+jianli1028@users.noreply.github.com>
2022-04-20 11:36:05 +0100

    updated controller, models, and routes

--- api/models/posts.js
+++ api/models/posts.js
@@ -26,9 +26,29 @@ module.exports = class Post {
     static async create(postData) {
         return new Promise (async (resolve, reject) =>{
             try {
-                const {title, author, story} = postData;
+                const {title, author, story, publicationDay} = postData;
+                //let post = await Post.findOrCreateByName(author);
+                let result = await db.query(`INSERT INTO posts (title, author, story, publicationDay)
+                                             VALUE ($1, $2, $3, $4)
+                                             RETURNING *;`, [title, author, story, publicationDay]);
+                resolve (result.rows[0]);
+            } catch(err) {
+                reject("Your story could not be created");
             }
-        })
-    }
+        });
+    };
 
-}
+    static async edit(postData) {
+        return new Promise (async (resolve, reject) => {
+            try {
+                const result = await db.query(`UPDATE *
+                                               FROM posts
+                                               WHERE id = $1
+                                               RETURNING author;`, [ this.id ]);
+                resolve (result.rows[0]);
+            } catch (err) {
+                reject("Post could not be updated")
+            }
+        });
+    };
+};
